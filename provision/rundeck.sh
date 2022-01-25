e28beed644fa134fc3c91b0f3ce16129db16c36b
Leonardo S Santos <rmnobarra@gmail.com>
2022-01-25 12:04:50 -0300

    script rundeck atualizado

--- provision/rundeck.sh
+++ provision/rundeck.sh
@@ -4,17 +4,21 @@ apt-get update
 apt-get install -y vim ansible openjdk-11-jdk gnupg2 curl git sqlite3
 
 # Rundeck
-#wget -q -O - 'https://bintray.com/user/downloadSubjectPublicKey?username=bintray' | apt-key add -
-#echo 'deb https://rundeck.bintray.com/rundeck-deb /' > /etc/apt/sources.list.d/rundeck.list
-curl -s https://packagecloud.io/install/repositories/pagerduty/rundeck/script.deb.sh | os=any dist=any bash
 
+curl -s https://packagecloud.io/install/repositories/pagerduty/rundeck/script.deb.sh | os=any dist=any bash
 apt-get update
 apt-get install -y rundeck rundeck-cli
 
-# Rundeck
+# Rundeck configs
+
 sed -i s/admin:admin/g /etc/rundeck/realm.properties
 sed -i s/localhost/172.27.11.10/g /etc/rundeck/framework.properties
 sed -i s/localhost/172.27.11.10/g /etc/rundeck/rundeck-config.properties
 
 systemctl enable rundeckd
 systemctl restart rundeckd
+
+# Clone do repo
+
+ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone git@github.com:rmnobarra/rundeck.git
